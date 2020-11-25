37feaa4c605153995b3dc55feb04e1db22e733ae
dudasenna <meduardasenna1@gmail.com>
2020-11-25 09:50:28 -0300

    inicia novo projeto

---
+++ Arya/Arya/AryaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AryaApp.swift
+//  Arya
+//
+//  Created by Eduarda Senna on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AryaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
