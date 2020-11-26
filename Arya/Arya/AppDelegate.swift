98a25ce5aae085ea76dc62a35aff709d2fca3c08
Ana Melo <anacsoaresmelo@gmail.com>
2020-11-25 21:35:34 -0300

    Add AppDelegate and SceneDelegate files.

---
+++ Arya/Arya/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Arya
+//
+//  Created by Ana Carolina Soares de Melo on 25/11/20.
+//
+
+import Foundation
---
+++ Arya/Arya/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  Arya
+//
+//  Created by Ana Carolina Soares de Melo on 25/11/20.
+//
+
+import Foundation
