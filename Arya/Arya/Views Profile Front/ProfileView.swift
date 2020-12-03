536c486ec669135c0001a2c4bcf1c716459ffa23
Ana Melo <anacsoaresmelo@gmail.com>
2020-12-03 10:21:41 -0300

    Modifies the colors from Navigation Bar

--- Arya/Arya/Views Profile Front/ProfileView.swift
+++ Arya/Arya/Views Profile Front/ProfileView.swift
@@ -24,6 +24,7 @@ struct ProfileView: View {
             .edgesIgnoringSafeArea(.all)
             .navigationBarHidden(true)
         }
+        .navigationBarColor(backgroundColor: .clear, tintColor: .orange)
     }
 }
 
