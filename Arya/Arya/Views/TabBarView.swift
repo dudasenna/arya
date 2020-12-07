23a39fe1b3132a3612bddc8cecc2e4d47bcc13ca
Ana Melo <anacsoaresmelo@gmail.com>
2020-12-07 12:09:33 -0300

    Creates default Tab Bar

---
+++ Arya/Arya/Views/TabBarView.swift
@@ -0,0 +1,50 @@
+//
+//  TabBarView.swift
+//  Arya
+//
+//  Created by Ana Carolina Soares de Melo on 07/12/20.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Início")
+                }.tag(0)
+            // Swap function Text("Quiz") for function below
+            //QuizView()
+            Text("Quiz")
+                .tabItem {
+                    Image(systemName: "gamecontroller.fill")
+                    Text("Quiz")
+                }.tag(1)
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Perfil")
+                }.tag(2)
+        }
+        .accentColor(.orange)
+    }
+}
+
+extension UITabBarController {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        let standardAppearance = UITabBarAppearance()
+//
+        standardAppearance.backgroundColor = .clear
+//
+        tabBar.standardAppearance = standardAppearance
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
