1305bc7fae9101f00b5b3983c0dddc64ace6dab5
Ana Melo <anacsoaresmelo@gmail.com>
2020-12-09 16:40:22 -0300

    Creates new branch feature/navigation-course

--- Arya/Arya/Views/Specific Course/Course.swift
+++ Arya/Arya/Views/Specific Course/Course.swift
@@ -12,34 +12,7 @@ struct CourseView: View {
     @EnvironmentObject var viewRouter: ViewRouter
     
     var body: some View {
-        //        VStack(alignment: .center, spacing: nil, content: {
-        //            Text("Curso especifico")
-        //            Button(action: {
-        //                self.viewRouter.currentPage = "homeView"
-        //            }, label: {
-        //                Text("Voltar")
-        //            })
-        //            .padding(.vertical)
-        //            Button(action: {
-        //                self.viewRouter.currentPage = "curiositiesView"
-        //            }, label: {
-        //                Text("Curiosidades")
-        //            })
-        //            .padding(.vertical)
-        //            Button(action: {
-        //                self.viewRouter.currentPage = "areasView"
-        //            }, label: {
-        //                Text("Áreas de atuação")
-        //            })
-        //            .padding(.vertical)
-        //            Button(action: {
-        //                self.viewRouter.currentPage = "aboutCourseView"
-        //            }, label: {
-        //                Text("Sobre o curso")
-        //            })
-        //            .padding(.vertical)
-        //        })
-        VStack(alignment: .center, spacing: nil, content: {
+        VStack(alignment: .center, spacing: nil) {
             HStack(alignment: .center) {
                 Button(action:{
                     self.viewRouter.currentPage = "tabBarView"
@@ -160,9 +133,10 @@ struct CourseView: View {
             }.frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity, minHeight: 0,
                     idealHeight: 100, maxHeight: .infinity, alignment: .center)
             Spacer()
-        })
+        }
     }
 }
+
 struct Course_Previews: PreviewProvider {
     static var previews: some View {
         CourseView()
