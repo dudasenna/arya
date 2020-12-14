04f4e1f1cfd8075b16602059fdb0fc8eaf99ddf1
Ana Melo <anacsoaresmelo@gmail.com>
2020-12-14 12:09:15 -0300

    Changes Profile page interface

--- Arya/Arya/Views/Supporting Views/ProfileHeaderView.swift
+++ Arya/Arya/Views/Supporting Views/ProfileHeaderView.swift
@@ -10,25 +10,13 @@ import SwiftUI
 struct ProfileHeaderView: View {
     var body: some View {
         ZStack {
-            //
-            VStack {
-                Ellipse()
-                    .frame(width: 600, height: 300, alignment: .center)
-                    .padding(.top, -70)
-                    .foregroundColor(.purple)
-                    .shadow(color: .purple, radius: 10)
-                    .accessibility(hidden: true)
-                Spacer()
-            }.edgesIgnoringSafeArea(.top)
-            //
             VStack {
                 Circle()
                     .frame(width: 120, height: 120, alignment: .center)
                     .foregroundColor(.white)
-                    .shadow(color: Color(.black), radius: 10).opacity(0.4)
+                    .shadow(color: Color(.white), radius: 10).opacity(0.4)
                 
-                Spacer()
-            }.padding(.top, 25)
+            }.padding(.bottom, 25)
             //
             VStack {
                 Image(systemName: "person.fill")
@@ -42,10 +30,9 @@ struct ProfileHeaderView: View {
                     title: { Text("\(namePassar)").font(.title).fontWeight(.heavy)
                     },
                     icon: {}
-                ).padding(.top, 15)
+                ).padding(.top, 20)
                 .foregroundColor(.white)
                 //
-                Spacer()
             }.padding(.top, 40)
         }.offset(y: 20)
     }
